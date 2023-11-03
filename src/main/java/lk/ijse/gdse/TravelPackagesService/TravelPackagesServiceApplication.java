b0aa126a8561c5875d61149a8b1e5e670b2eaeb2
chamoddinuranga <chamod3246@gmail.com>
2023-11-03 14:29:01 +0530

    init travel package service

---
+++ src/main/java/lk/ijse/gdse/TravelPackagesService/TravelPackagesServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.TravelPackagesService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPackagesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPackagesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/gdse/TravelPackagesService/TravelPackagesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.TravelPackagesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPackagesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
